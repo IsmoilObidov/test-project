330741de25db18dc3c952102c874050f2e731c72
Ismoil <84452090+IsmoilObidov@users.noreply.github.com>
2024-07-16 12:48:23 +0500

    db initialized and config installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,27 @@ import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './config/middleware/logger.middleware';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { databaseProviders } from './database/database.providers';
 
 @Module({
-  imports: [CatsModule, AuthModule, UsersModule],
+  imports: [CatsModule, AuthModule, UsersModule, DatabaseModule],
+  providers: [
+    {
+      // actions before app starts
+      provide: 'ASYNC_CONNECTION',
+      useFactory: async () => {
+        for (let i = 0; i < 10; i++) {
+          console.log(i);
+        }
+        console.log(databaseProviders);
+        
+      },
+    }
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-   consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
   }
 }
